f1edc3d2f313e9accc874a51c6f6659b1fc343ac
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2024-08-09 16:43:52 +0900

    main.js

---
+++ front-web/static/javascript/main/main.js
@@ -0,0 +1,12 @@
+const btn1 = document.getElementsByClassName("btn1")[0];
+const btn2 = document.getElementsByClassName("btn2")[0];
+
+// 재고 관리 버튼
+btn1.onclick = () => {
+  location.href = "/"; // 추후 경로 작성
+};
+
+// 추천 메뉴 버튼
+btn2.onclick = () => {
+  location.href = "/"; // 추후 경로 작성
+};
