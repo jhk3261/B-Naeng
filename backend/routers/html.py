67cdc9bc8a539fdfc6b1f18eedc08ca898bcacd3
Seo Jiwon <wldnjstj99@gmail.com>
2024-08-10 04:00:10 +0900

    stocklist slide

--- backend/routers/html.py
+++ backend/routers/html.py
@@ -20,3 +20,9 @@ async def main(request: Request):
 @router.get("/mobile_authenticated", response_class=HTMLResponse)
 async def mobile_authenticated(request: Request):
     return templates.TemplateResponse("QR/mobileQRResponse.html", {"request": request})
+
+
+# 재고 리스트
+@router.get("/stock_list", response_class=HTMLResponse)
+async def stock_list(request : Request):
+    return templates.TemplateResponse("stock/stockList.html", {"request" : request})
