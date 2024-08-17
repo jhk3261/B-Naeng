b5d8d41ff5ea0dc4e008a5c75d89cc4e6ce128ca
MinJae Lee <wjdchs0129@gnu.ac.kr>
2024-08-18 07:47:46 +0900

    ㅁㄴㅇ

--- front-web/static/javascript/main/main.js
+++ front-web/static/javascript/main/main.js
@@ -1,12 +1,11 @@
-const btn1 = document.getElementsByClassName("btn1")[0];
-const btn2 = document.getElementsByClassName("btn2")[0];
+let btn1 = document.getElementsByClassName("btn1")[0];
+let btn2 = document.getElementsByClassName("btn2")[0];
 
 // 재고 관리 버튼
-btn1.onclick = () => {
-  location.href = "/"; // 추후 경로 작성
-};
+btn1.addEventListener('click',  () => {
+  location.href = "/stock_list"
+})
 
-// 추천 메뉴 버튼
-btn2.onclick = () => {
-  location.href = "/"; // 추후 경로 작성
-};
+btn2.addEventListener('click',  () => {
+  location.href = "/recommend"
+})
