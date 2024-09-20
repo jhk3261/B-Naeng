580ddc781b5ce88b48e66edc0a2ed455ed03c406
Seo Jiwon <wldnjstj99@gmail.com>
2024-09-20 13:07:47 +0900

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:frontend/Pages/receipe_recommend.dart';
-// import 'package:frontend/Pages/friger.dart';
+import 'package:frontend/Pages/friger.dart';
 import 'package:frontend/Pages/signup.dart'; // github에 올릴 때 지우기
 // import 'package:frontend/Pages/google_login.dart';
 import 'package:frontend/Pages/google_auth.dart';
@@ -13,21 +13,9 @@ void main() async {
 
   runApp(MaterialApp(
     theme: ThemeData(
-<<<<<<< HEAD
-        textTheme: const TextTheme(
-      bodyLarge: TextStyle(fontFamily: 'GmarketSansLight'),
-      bodyMedium: TextStyle(fontFamily: 'GmarketSansMedium'),
-      headlineLarge: TextStyle(fontFamily: 'GmarketSansBold'),
-    )),
-    // home: Friger(cameras: cameras),
-    // home: GoogleLoginPage(),
-    home: SignInPage(),
-    // home: ReceipeRecommend(cameras: cameras),
-=======
       fontFamily: 'GmarketSansMedium',
     ),
     themeMode: ThemeMode.system,
     home: Friger(cameras: cameras),
->>>>>>> main
   ));
 }
