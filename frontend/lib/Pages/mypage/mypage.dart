2a4f53d3964df2b8224e76442b9175b14263461d
JH <jhk3261@gnu.ac.kr>
2024-09-25 17:58:08 +0900

    냉장고 api 수정

--- frontend/lib/Pages/mypage/mypage.dart
+++ frontend/lib/Pages/mypage/mypage.dart
@@ -4,7 +4,6 @@ import 'package:frontend/Pages/mypage/fridge_popup.dart';
 import 'package:http/http.dart' as http;
 import 'package:camera/camera.dart';
 import 'user_points_page.dart';
-import 'dart:convert';
 
 const String apiUrl = 'http://127.0.0.1:8000';
 
